69de345e11ddc7c6da03a84db0984afb502f2b2a
Luk1Pr0 <lpytel16@gmail.com>
2021-02-11 17:22:03 +0100

    Changes to the country component and styling

--- src/components/Country.js
+++ src/components/Country.js
@@ -11,15 +11,19 @@ const Country = ({ country }) => {
 				<h1 style={headerStyle}><span style={labelStyle}>Country: </span>{Country}</h1>
 			</div>
 			<div className="content__container">
-				<div className="left__content">
+				<div className="left__content content__wrapper">
 					<h3><span style={labelStyle}>New Cases: </span>{NewConfirmed}</h3>
+					<hr />
 					<h3><span style={labelStyle}>New Deaths: </span>{NewDeaths}</h3>
-					<h3><span style={labelStyle}>New Recoveries: </span>{NewRecovered}</h3>
+					<hr />
+					<h3><span style={labelStyle}>Recovered: </span>{NewRecovered}</h3>
 				</div>
-				<div className="right__content">
+				<div className="right__content content__wrapper">
 					<h3><span style={labelStyle}>Total Cases: </span>{TotalConfirmed}</h3>
+					<hr />
 					<h3><span style={labelStyle}>Total Deaths: </span>{TotalDeaths}</h3>
-					<h3><span style={labelStyle}>Total Recoveries: </span>{TotalRecovered}</h3>
+					<hr />
+					<h3><span style={labelStyle}>Recovered: </span>{TotalRecovered}</h3>
 				</div>
 			</div>
 		</div>
