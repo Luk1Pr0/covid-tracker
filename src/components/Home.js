1dfbc8aa375e24721c3fd2cf540ab7d7b163f81e
Luk1Pr0 <lpytel16@gmail.com>
2021-02-16 11:25:29 +0100

    Added the last updated date functionality to the app

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,18 @@ import React from 'react';
 
 const Home = ({ data, date }) => {
 
-	// const formattedDate = newDate[0].split('-').reverse().join('/');
-	// const formattedTime = newDate[1].split('.')[0].split(':').slice(0, 2).join(':');
-
 	const formatDate = () => {
-		const formattedDate = date[0].split('-').reverse().join('/');
-		const formattedTime = date[1].split('.')[0].split(':').slice(0, 2).join(':');
-		const finalDate = `${formattedDate} at ${formattedTime}`
-		return finalDate;
+		if (typeof date !== 'string') {
+			const formattedDate = date[0].split('-').reverse().join('/');
+			const formattedTime = date[1].split('.')[0].split(':').slice(0, 2).join(':');
+			const finalDate = `${formattedDate} at ${formattedTime}`
+			return finalDate;
+		}
 	}
 
 	return (
 		<div style={homeStyle}>
-			{/* <h4><span style={label}>Last Updated: <br /></span>{formatDate()}</h4> */}
+			<h4><span style={dateLabel}>Last Updated: <br /></span>{formatDate()}</h4>
 			<h3><span style={label}>Total Cases: </span>{data.TotalConfirmed}</h3>
 			<h3><span style={label}>Total Deaths: </span>{data.TotalDeaths}</h3>
 			<h3><span style={label}>Total Recoveries: </span>{data.TotalRecovered}</h3>
@@ -33,6 +32,10 @@ const homeStyle = {
 
 const label = {
 	color: 'gold',
+}
+
+const dateLabel = {
+	color: 'aquamarine'
 }
 
 export default Home;
